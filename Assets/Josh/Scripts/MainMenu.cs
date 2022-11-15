ef22d640b14c2c064a8768716ef888442d8bb8c3
JoshEu <joshjbrown706@gmail.com>
2022-11-15 20:13:18 +0000

    Josh 7th Commit - Main Menu Credit tweaks - enter goes back to menu

--- Assets/Josh/Scripts/MainMenu.cs
+++ Assets/Josh/Scripts/MainMenu.cs
@@ -1,7 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    [SerializeField]
+    private InputActionReference enterInput;
+    [SerializeField]
+    private GameObject MainMenuObj;
+    [SerializeField]
+    private GameObject CreditsObj;
+
+    private float enterValue;
+    private bool isEnterPressed;
+
+    private void Awake() {
+        enterInput.action.performed += context => enterValue = context.action.ReadValue<float>();
+    }
+    private void Update() {
+        if (Keyboard.current.enterKey.isPressed) {
+            CreditsObj.SetActive(false);
+            MainMenuObj.SetActive(true);
+        }
+    }
     public void StartButton() {
         Time.timeScale = 1;
         // Load Level One Scene 
@@ -10,9 +30,6 @@ public class MainMenu : MonoBehaviour {
     public void OptionsButton() {
         // Load Option Menu Scene 
         SceneManager.LoadScene("OptionsMenu");
-    }
-    public void CreditsButton() {
-
     }
     public void QuitButton() {
         // Delete this line later!!!!
